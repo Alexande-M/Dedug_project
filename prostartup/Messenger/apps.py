e760b008c92ab7c9df99095c28751466bbabaa62
Alexander <60608888+Alexander-Mochinov@users.noreply.github.com>
2020-06-30 00:09:16 +0400

    Delete

--- prostartup/Messenger/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class MessengerConfig(AppConfig):
-    name = 'Messenger'
-
-    def ready(self):
-        import Messenger.receivers
--- prostartup/Payment/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Personalaccount, Operations
-
-
-@admin.register(Personalaccount)
-class Personal_accountAdmin(admin.ModelAdmin):
-    pass
